9411dcaf8f75138b3b11b1a8b79c8decd3db80d7
Stephen Dimmick <stephen.dimmick@gmail.com>
2011-12-08 17:50:20 -0500

    initial, functional commit with some error handling

---
+++ README.md
@@ -0,0 +1,15 @@
+Stupid simple JSONP proxy. Currently deployable to Google App Engine, but it's
+all just plain old, portable Python.
+
+Request Parameters
+==================
+
+* callback - Name of the Javascript function to wrap around the response
+* url      - URL of the JSON web service
+
+Example
+=======
+
+http://jsonpme.appspot.com/?callback=foo&url=http://some.json.endpoint.here.com
+
+returns --> 'foo({"key": "val"})'
---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import urllib
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+
+class MainHandler(webapp.RequestHandler):
+    def get(self):
+        # query string parameters
+        url = self.request.get('url')
+        callback = self.request.get('callback')
+        
+        if len(url) == 0 or len(callback) == 0:
+            self.response.out.write('usage: http://jsonpme.appspot.com/?callback=[function_name]&url=[json_url]')
+        else:
+            try:
+                # open the URL
+                f = urllib.urlopen(url)
+                json = f.read()
+        
+                # wrap the JSON in a function call
+                jsonp = callback + '(' + json + ')'
+        
+                # write response
+                self.response.headers.add_header('Content-Type', 'text/javascript')
+                self.response.out.write(jsonp)
+            except IOError:
+                self.response.set_status(500)
+                self.response.out.write("Error opening URL: " + url)
+
+def main():
+    application = webapp.WSGIApplication([('/', MainHandler)], debug=True)
+    util.run_wsgi_app(application)
+
+if __name__ == '__main__':
+    main()
